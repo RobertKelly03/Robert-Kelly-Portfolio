fcda55560317a17d4a68521cf18dbb106c945aa8
Robert Kelly <rk078701@gmail.com>
2022-02-15 21:56:10 -0800

    adding dark mode feature

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 
 
 document.getElementById('light').onclick = function() {
-    
+    document.getElementById('main').style.backgroundColor = "white";
+}
+
+document.getElementById('dark').onclick = function() {
+    document.getElementById('main').style.backgroundColor = "black";
 }
